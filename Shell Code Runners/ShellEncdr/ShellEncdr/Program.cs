ff03af4f61400f3f5676ac9e84fd7ec13c0248a5
Raúl Redondo <raul.redondo.t@gmail.com>
2021-10-19 18:53:40 +0200

    Up

---
+++ Shell Code Runners/ShellEncdr/ShellEncdr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShellEncdr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
